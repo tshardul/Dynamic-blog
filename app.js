5c102c3c9502e7c821b8d88394b4ba3e5eeb58a2
tshardul <shardult16@gmail.com>
2020-04-30 14:31:04 +0530

    Second commit

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post("/compose",function(req,res){
       res.redirect("/");
     }
   });
-  
+
 });
 
 app.get("/posts/:postId", function(req,res){
@@ -75,8 +75,11 @@ app.get("/posts/:postId", function(req,res){
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started succesfully");
 });
